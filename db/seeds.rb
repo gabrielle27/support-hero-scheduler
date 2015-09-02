cb288a042b8ec29899cb656955197fca3490c7fa
gabrielle27 <gabrielledewitt@gmail.com>
2015-09-01 23:17:27 -0400

    Seed California Holidays
    Add 2015/216 CA Holidays to
    db seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+## seed the CA holidays
+
+{
+  "2015-09-07" => "Labor Day",
+  "2015-09-25" => "Native American Day",
+  "2015-11-11" => "Veterans' Day",
+  "2015-11-26" => "Thanksgiving Day",
+  "2015-11-27" => "Thanksgiving Friday",
+  "2015-12-25" => "Christmas Day",
+  "2016-01-01" => "New Year's Day",
+  "2016-01-18" => "Martin Luther King Jr. Birthday",
+  "2016-02-04" => "Rosa Parks Day",
+  "2016-02-15" => "President's Day",
+  "2016-03-31" => "César Chávez Day",
+  "2016-05-30" => "Memorial Day",
+  "2016-07-04" => "Independence Day",
+  "2016-09-05" => "Labor Day",
+  "2016-09-23" => "Native American Day",
+  "2016-11-11" => "Veterans' Day",
+  "2016-11-24" => "Thanksgiving Day",
+  "2016-11-25" => "Thanksgiving Friday",
+  "2016-12-25" => "Christmas Day",
+  "2016-12-26" => "Christmas Day (Observed)"
+}.each do |date, name|
+  Holiday.create(support_date: date, name: name)
+end
