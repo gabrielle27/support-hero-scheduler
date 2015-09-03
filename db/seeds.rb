ca4c1d5ca3a359c77795efa16d89c4349b404239
gabrielle27 <gabrielledewitt@gmail.com>
2015-09-02 20:35:27 -0400

    Seed Schedule Data From List

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,8 @@
 }.each do |date, name|
   Holiday.create(support_date: date, name: name)
 end
+
+Schedule.from_list( %w(Sherry Boris Vicente Matte Jack Sherry Matte Kevin
+  Kevin Vicente Zoe Kevin Matte Zoe Jay Boris Eadon Sherry Franky Sherry Matte
+  Franky Franky Kevin Boris Franky Vicente Luis Eadon Boris Kevin Matte Jay
+  James Kevin Sherry Sherry Jack Sherry Jack) )
